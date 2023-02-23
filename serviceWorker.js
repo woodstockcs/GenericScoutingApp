e854e92b052b63e91c2aca5bc7d2c27bd50f3130
andytilia <andytilia@users.noreply.github.com>
2023-02-23 13:39:07 -0500

    Create serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticScouting = "archytas-scouting-app-v1"
+const assets = [
+  "/",
+  "p5.min.js",
+  "/p5testing.html",
+  "/p5testing.js",
+  "/512.png",
+  "/192.png"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticScouting).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+})
