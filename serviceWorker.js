0ee77d5979ca3968e404985ef6d8970feb784137
andy <andytilia@gmail.com>
2023-03-10 19:22:36 -0500

    add more js to serviceworker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,14 @@
 const staticScouting = "archytas-scouting-app-v1"
 const assets = [
+  "css/bootstrap.min.css",
+  "css/style.css",
+  "js/bootstrap.min.js",
+  "js/jquery-3.4.1.min.js",
+  "js/p5.min.js",
+  "js/popper.min.js",
+  "js/qr-creator.js",
+  "js/qrcode.min.js",
   "/",
-  "p5.min.js",
   "/index.html",
   "/app.js",
   "/512.png",
